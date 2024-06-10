5dc065125eb44eb313f56e54690657ac43aa67b7
paoloficaraa <paoloficara50@gmail.com>
2024-06-10 16:07:31 +0200

    finalmente l'audio starta

--- app/src/main/java/it/insubria/accordi/MainActivity.kt
+++ app/src/main/java/it/insubria/accordi/MainActivity.kt
@@ -1,22 +1,14 @@
 package it.insubria.accordi
 
-import android.media.AudioFormat
-import android.media.AudioRecord
-import android.media.MediaRecorder
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import be.tarsos.dsp.AudioEvent
-import be.tarsos.dsp.io.jvm.AudioDispatcherFactory
-import be.tarsos.dsp.pitch.PitchDetectionHandler
-import be.tarsos.dsp.pitch.PitchDetectionResult
-import be.tarsos.dsp.pitch.PitchProcessor
 import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
-//    val database = FirebaseDatabase.getInstance()
+    val database = FirebaseDatabase.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
