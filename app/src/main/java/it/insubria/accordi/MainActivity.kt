6fd60493734e3eae5ba33c4190be01f7ed84ecac
paoloficaraa <paoloficara50@gmail.com>
2024-06-10 16:27:52 +0200

    proseguire

--- app/src/main/java/it/insubria/accordi/MainActivity.kt
+++ app/src/main/java/it/insubria/accordi/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import be.tarsos.dsp.io.android.AndroidAudioInputStream
 import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
@@ -19,5 +20,6 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         val audioRecordResult = initAudioRecord(this)
+        val mInputStream = AndroidAudioInputStream(audioRecordResult!!.audioRecord, audioRecordResult.format)
     }
 }
